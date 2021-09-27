6bd3269dd9cb951e3dd6e32bab93d6557acaa1fc
Sandro Leon <zandrotja@gmail.com>
2021-09-27 16:47:31 -0400

    v2

--- newmedic-view.php
+++ newmedic-view.php
@@ -7,21 +7,23 @@ $categories = CategoryData::getAll();
 
 <div class="card">
   <div class="card-header" data-background-color="blue">
-      <h4 class="title">Nuevo Medico</h4>
+
+
+			<h4 class="title">Nuevo Medico</h4>
   </div>
-  <div class="card-content table-responsive">
+  <div class= "card-content table-responsive">
 		<form class="form-horizontal" method="post" id="addproduct" action="index.php?view=addmedic" role="form">
 
   <div class="form-group">
-		
+
 	</div>
     <label for="inputEmail1" class="col-lg-2 control-label">Area*</label>
     <div class="col-md-6">
     <select name="category_id" class="form-control">
-    <option value="">-- SELECCIONE --</option>
-    <?php foreach($categories as $cat):?>
-    <option value="<?php echo $cat->id; ?>"><?php echo $cat->name; ?></option>
-    <?php endforeach;?>
+			    <option value="">-- SELECCIONE --</option>
+			    <?php foreach($categories as $cat):?>
+			    <option value="<?php echo $cat->id; ?>"><?php echo $cat->name; ?></option>
+			    <?php endforeach;?>
     </select>
     </div>
   </div>
@@ -32,18 +34,21 @@ $categories = CategoryData::getAll();
       <input type="text" name="name" class="form-control" id="name" placeholder="Nombre">
     </div>
   </div>
+
   <div class="form-group">
     <label for="inputEmail1" class="col-lg-2 control-label">Apellido*</label>
     <div class="col-md-6">
       <input type="text" name="lastname" required class="form-control" id="lastname" placeholder="Apellido">
     </div>
   </div>
+
   <div class="form-group">
     <label for="inputEmail1" class="col-lg-2 control-label">Direccion*</label>
     <div class="col-md-6">
       <input type="text" name="address" class="form-control"  id="address" placeholder="Direccion">
     </div>
   </div>
+
   <div class="form-group">
     <label for="inputEmail1" class="col-lg-2 control-label">Email*</label>
     <div class="col-md-6">
@@ -57,11 +62,13 @@ $categories = CategoryData::getAll();
       <input type="text" name="phone" class="form-control" id="phone" placeholder="Telefono">
     </div>
   </div>
+
   <div class="form-group">
     <div class="col-lg-offset-2 col-lg-10">
       <button type="submit" class="btn btn-primary">Agregar Medico</button>
     </div>
   </div>
+
 </form>
 </div>
 </div>
